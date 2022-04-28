47e5ecb591332fd133b271c038e7333aac9cfdb3
Luca Coduri <coduri.vitor@gmail.com>
2022-04-28 21:35:21 +0200

    refactor + show

--- json.hs
+++ json.hs
@@ -1,5 +1,6 @@
 import Data.Char
 import System.Environment
+import Control.Exception
 
 
 -- testInput = "{ " ++ '"':"str" ++ '"' : ": [ 123, true, null ] }"
@@ -22,77 +23,83 @@ import System.Environment
 --     where (token,rest) = span isAlpha cs
 
 
-
-
-isSpecial c = elem c ":"
-
-data Token = JKeyValue (Token, Token) | JObject [Token] | JNumber Int | JString String | JBool Bool | JNull | JArray [Token] | JSpecial String | JError String
+data JsonException = WrongFileFormat String
     deriving (Show)
+instance Exception JsonException
+
+
+data Token = JKeyValue (Token, Token) | JObject [Token] | JArray [Token] | JNumber Int | JString String | JBool Bool | JNull | JSpecial String | JError String
+
+
+instance Show Token where
+    show (JNumber x) = show x
+    show (JString s) = show s
+    show (JBool b) = show b
+    show JNull = show "null"
+    show (JSpecial s) = show s
+    show (JKeyValue (key, value)) = show key ++ ": " ++ show value
+    show (JObject ts) = (foldl (\acc x -> acc ++ (show x) ++ ", ") "{" ts) ++ "}"
+    show (JArray ts) = show ts
+
 lexer :: String -> [Token]
 lexer cs =
     let
+        -- permet de créer les clés valeurs (JKeyValue)
         f:: [Token] -> [Token]
         f [] = []
         f ((JObject a):xs) = JObject (f a): (f xs) 
         f ((JArray a):xs) = JObject (f a): (f xs) 
+        f (a:(JSpecial _):(JObject c):xs) = (JKeyValue (a,JObject(f c))):(f xs)
+        f (a:(JSpecial _):(JArray c):xs) = (JKeyValue (a,JArray(f c))):(f xs)
         f (a:(JSpecial _):c:xs) = (JKeyValue (a,c)):(f xs)
         f (a:xs) = a:(f xs)
-        value = f (fst (lexer' cs))
     in
-        value
+        f (lexer' cs)
 
-lexer' :: String -> ([Token], String)
-lexer' [] = ([], "")
+-- Analyse lexical d'un json
+lexer' :: String -> [Token]
+lexer' [] = []
 lexer' (c:cs)
-    | isSpace c = (fst (lexer' cs), "")
-    | ',' == c = (fst (lexer' cs), "")
-    | '[' == c = (onArray cs, "")
-    | '"' == c = (onString cs, "")
-    | '\'' == c = (onString2 cs,"")
-    | '{' == c = (onObject cs, "")
-    | isSpecial c = (token JSpecial isSpecial (c:cs), "")
-    | isAlpha c = (onAlpha (c:cs),"")
-    | isDigit c = (onDigit (c:cs), "")
-    | '}' == c = ([], (cs))
-    | ']' == c = ([], (cs))
-    | otherwise = (JError(show (c:cs)) : [], "")
+    | isSpace c = (lexer' cs)
+    | ',' == c = (lexer' cs)
+    | '[' == c = (arrayToken JArray (/=']') cs)
+    | '"' == c = (token JString (/='"') cs)
+    | '\'' == c = (token JString (/='\'') cs)
+    | '{' == c = (arrayToken JObject (/='}') cs)
+    | ':' == c = (JSpecial ":") : lexer' cs 
+    | isAlpha c = (onAlpha (c:cs))
+    | isDigit c = (onDigit (c:cs))
+    | '}' == c = []
+    | ']' == c = []
+    | otherwise = throw (WrongFileFormat ("on: " ++ [c]))
 
-token constructor filter cs = constructor token: fst (lexer' rest)
-    where (token, rest) = span filter cs
 
-onString2 cs = JString (token) : fst (lexer' rest)
-    where (token, (r:rest)) = span (/= '\'') cs
+token constructor filter cs = constructor (token) : (lexer' rest)
+    where (token, (r:rest)) = span filter cs
 
-onString cs = JString (token) : fst (lexer' rest)
-    where (token, (r:rest)) = span (/= '"') cs
-
-onArray cs = 
+-- Construction des objets ou tableau
+arrayToken constructor filter cs = 
     let 
-        (value, other) = lexer' cs
+        (_, (_:rest)) = span filter cs
+        value = lexer' cs
     in
-        JArray (value) : fst (lexer' other)
+        (constructor value) : (lexer' rest)
 
-onObject cs = 
-    let
-        (value, other) = lexer' cs
-    in
-        JObject (value) : fst (lexer' other)
-        
-
-onDigit cs = JNumber (read token :: Int): fst (lexer' rest)
+-- Conversion des nombres en Int
+onDigit cs = JNumber (read token :: Int): (lexer' rest)
     where (token, rest) = span isDigit cs
 
+-- Check des bool et null
 onAlpha cs =
     let
         onAlpha' os
             | os == "true" = JBool True
             | os == "false" = JBool False
             | os == "null" = JNull
-            | otherwise = JError os
+            | otherwise = throw (WrongFileFormat ("on: " ++ os))
     in
-        onAlpha' token :  fst (lexer' rest)
+        onAlpha' token :  (lexer' rest)
     where (token, rest) = span isAlpha cs
-    
 
 main::IO()
 main = do
