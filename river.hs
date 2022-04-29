ecd0ddf934a62b3c54deb8c7897f3c39dad5ebca
Luca Coduri <coduri.vitor@gmail.com>
2022-04-29 12:26:30 +0200

    Commentaires

--- river.hs
+++ river.hs
@@ -1,6 +1,10 @@
 {-
 Labo 1 - Exercice 2.5
 Auteurs : Luca Coduri & Chloé Fontaine
+
+remarques:
+    Lorsqu'un joueur souhaite faire un mouvement non autorisé, le mouvement n'est tout simplement pas effectué.
+    Il n'est donc pas possible de perdre à ce jeu.
 -}
 
 
@@ -11,25 +15,29 @@ data Side = MyLeft | MyRight deriving (Eq, Show)
 data Boat = Boat{ side::Side, passenger::Passenger } deriving (Show)
 data GameState = GameState { left :: [Passenger], right :: [Passenger], boat:: Boat } deriving (Show)
 
+-- Charger le bateau.
 load:: GameState -> Passenger -> GameState
 load gs None = gs
 load gs@(GameState _ [] (Boat MyRight _)) _ = gs
 load gs@(GameState [] _ (Boat MyLeft _)) _ = gs
 load (GameState le ri (Boat MyLeft None)) p = GameState (filter (/= p) le) ri (Boat MyLeft p)
 load (GameState le ri (Boat MyRight None)) p = GameState le (filter (/= p) le) (Boat MyRight p)
--- quand le bateau est plein on échange les places
+-- Quand le bateau est plein on échange les places.
 load (GameState le ri (Boat MyLeft onboard)) p = GameState (onboard:(filter (/= p) le)) ri (Boat MyLeft p)
 load (GameState le ri (Boat MyRight onboard)) p = GameState le (onboard:(filter (/= p) ri)) (Boat MyRight p)
 
+-- Décharger le bateau.
 unload:: GameState -> GameState
 unload gs@(GameState le ri (Boat side None)) = gs
 unload gs@(GameState le ri (Boat MyLeft onboard)) = GameState (onboard:le) ri (Boat MyLeft None)
 unload gs@(GameState le ri (Boat MyRight onboard)) = GameState le (onboard:ri) (Boat MyRight None)
 
+-- Permet de déplacer le bateau à gauche ou à droite.
 moveBoat:: GameState -> GameState
 moveBoat (GameState le ri (Boat MyLeft onboard)) = GameState le ri (Boat MyRight onboard)
 moveBoat (GameState le ri (Boat MyRight onboard)) = GameState le ri (Boat MyLeft onboard)
 
+-- Vérifie si l'état d'une rive est autorisée.
 isLegal:: [Passenger] -> Bool
 isLegal [] = True
 isLegal [Wolf] = True
@@ -37,12 +45,14 @@ isLegal [Cabbage] = True
 isLegal [Sheep] = True
 isLegal gs = not ((elem Wolf gs) && (elem Sheep gs) && (elem Cabbage gs) || (elem Wolf gs) && (elem Sheep gs) || (elem Sheep gs) && (elem Cabbage gs))
 
+-- Converti une string en passenger.
 strToPassenger:: String -> Passenger
 strToPassenger "loup" = Wolf
 strToPassenger "chevre" = Sheep
 strToPassenger "chou" = Cabbage
 strToPassenger _ = None
 
+-- Affiche l'aide.
 printHelp :: IO()
 printHelp = do
     putStrLn ":p afficher l'état du jeu\n\
@@ -53,12 +63,18 @@ printHelp = do
             \:q quitter le jeu\n\
             \:h afficher l'aide"
 
+-- L'état de base d'une partie
 initState = GameState [Wolf, Sheep, Cabbage] [] (Boat MyLeft None)
 
 main:: IO()
 main = do
     mainLoop initState
-
+{-
+    La boucle principale du jeu
+    Elle attend les entrées utilisateurs puis execute l'action qui correspond.
+    Arguments:
+        1. L'etat du jeu
+-}
 mainLoop:: GameState -> IO()
 mainLoop (GameState [] _ (Boat _ None)) = do putStrLn "Vous avez gagné !"
 mainLoop gs = do
